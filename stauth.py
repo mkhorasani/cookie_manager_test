6bde27c94d80e2ff3cf54c2e37a070e8391dd2f3
M Khorasani <khorasani.mohammad@gmail.com>
2021-11-14 11:12:52 +0300

    Update stauth.py

--- stauth.py
+++ stauth.py
@@ -1,5 +1,5 @@
 import streamlit as st
-#import streamlit_authenticator as stauth
+import streamlit_authenticator as stauth
 
 names = ['John','Rebecca']
 usernames = ['js','rb']
@@ -7,14 +7,14 @@ passwords = ['12','45']
 
 st.title('Test')
 
-#hashed_passwords = stauth.hasher(passwords).generate()
+hashed_passwords = stauth.hasher(passwords).generate()
 
-#authenticator = stauth.authenticate(names,usernames,hashed_passwords,'stauths','12d3',cookie_expiry_days=0)
-#name, authentication_status = authenticator.login('Logins','sidebar')
+authenticator = stauth.authenticate(names,usernames,hashed_passwords,'stauths','12d3',cookie_expiry_days=0)
+name, authentication_status = authenticator.login('Logins','sidebar')
 
-#if authentication_status:
-#    st.write('Welcome *%s*' % (name))
-#elif authentication_status == False:
-#    st.error('Username/password is incorrect')
-#elif authentication_status == None:
-#    st.warning('Please enter your username and password')
+if authentication_status:
+    st.write('Welcome *%s*' % (name))
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
