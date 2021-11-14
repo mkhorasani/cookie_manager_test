a92e5e6a327b0b3c6885dc13ba171b3182ef31cc
M Khorasani <khorasani.mohammad@gmail.com>
2021-11-14 10:10:49 +0300

    Update stauth.py

--- stauth.py
+++ stauth.py
@@ -5,14 +5,16 @@ names = ['John','Rebecca']
 usernames = ['js','rb']
 passwords = ['12','45']
 
+st.title('Test')
+
 hashed_passwords = stauth.hasher(passwords).generate()
 
-authenticator = stauth.authenticate(names,usernames,hashed_passwords,'stauths','12d3',cookie_expiry_days=0)
-name, authentication_status = authenticator.login('Logins','sidebar')
+#authenticator = stauth.authenticate(names,usernames,hashed_passwords,'stauths','12d3',cookie_expiry_days=0)
+#name, authentication_status = authenticator.login('Logins','sidebar')
 
-if authentication_status:
-    st.write('Welcome *%s*' % (name))
-elif authentication_status == False:
-    st.error('Username/password is incorrect')
-elif authentication_status == None:
-    st.warning('Please enter your username and password')
+#if authentication_status:
+#    st.write('Welcome *%s*' % (name))
+#elif authentication_status == False:
+#    st.error('Username/password is incorrect')
+#elif authentication_status == None:
+#    st.warning('Please enter your username and password')
